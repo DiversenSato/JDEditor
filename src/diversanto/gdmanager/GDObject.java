d4196b6e4db0e339660120293d281334b97da916
DiversenSato <diversensato@gmail.com>
2022-09-12 23:02:21 +0200

    Move trigger done

--- src/diversanto/gdmanager/GDObject.java
+++ src/diversanto/gdmanager/GDObject.java
@@ -18,10 +18,18 @@ public class GDObject {
     private int blue;
     private float moveTime = -1;
     private boolean touchTriggered = false;
+    private boolean usePlayerCol1 = false;
+    private boolean usePlayerCol2 = false;
+    private boolean useBlending = false;
     private int editorLayer1 = 0;
-    private int colorChannel = -1;
+    private int baseColorChannel = -1;
+    private int detailColorChannel = -1;
+    private int colorTargetID = -1;
     private int zLayer = 0;
     private Integer zOrder = null;
+    private int moveX = 0;
+    private int moveY = 0;
+    private int easingMode = 0;
     private String text = null;
     private float scale = 1;
     private float opacity = -1;
@@ -30,16 +38,30 @@ public class GDObject {
     private float fadeIn = -1;
     private float fadeHold = -1;
     private float fadeOut = -1;
+    private HSB copyColorHSB = null;
+    private int copyColorChannel = -1;
     private int targetGroupID = -1;
     private final ArrayList<Integer> groups = new ArrayList<>();
+    private boolean lockPlayerX = false;
+    private boolean lockPlayerY = false;
     private int editorLayer2 = 0;
     private boolean spawnTriggered = false;
+    private boolean dontFade = false;
+    private boolean dontEnter = false;
+    private int targetPosID = -1;
+    private float easingRate = -1;
     private boolean noGlow = false;
+    private boolean useTarget = false;
+    private int xOrY = 0;
+    private boolean highDetail = false;
+    private int linkedID = -1;
 
     public GDObject(int id, int x, int y) {
         this.id = id;
         this.x = x;
         this.y = y;
+        dontFade = true;
+        dontEnter = true;
     }
 
     protected GDObject(String data) {
@@ -62,25 +84,45 @@ public class GDObject {
                 case 9 -> green = Integer.parseInt(value);
                 case 10 -> moveTime = Float.parseFloat(value);
                 case 11 -> touchTriggered = true;
+                case 15 -> usePlayerCol1 = true;
+                case 16 -> usePlayerCol2 = true;
+                case 17 -> useBlending = true;
                 case 20 -> editorLayer1 = Integer.parseInt(value);
-                case 21 -> colorChannel = Integer.parseInt(value);
+                case 21 -> baseColorChannel = Integer.parseInt(value);
+                case 22 -> detailColorChannel = Integer.parseInt(value);
+                case 23 -> colorTargetID = Integer.parseInt(value);
                 case 24 -> zLayer = Integer.parseInt(value);
                 case 25 -> zOrder = Integer.parseInt(value);
+                case 28 -> moveX = Integer.parseInt(value);
+                case 29 -> moveY = Integer.parseInt(value);
+                case 30 -> easingMode = Integer.parseInt(value);
                 case 32 -> scale = Float.parseFloat(value);
                 case 35 -> opacity = Float.parseFloat(value);
                 case 36 -> isTrigger = true;
                 case 45 -> fadeIn = Float.parseFloat(value);
                 case 46 -> fadeHold = Float.parseFloat(value);
                 case 47 -> fadeOut = Float.parseFloat(value);
+                case 49 -> copyColorHSB = new HSB(value);
+                case 50 -> copyColorChannel = Integer.parseInt(value);
                 case 51 -> targetGroupID = Integer.parseInt(value);
                 case 57 -> {
                     for (String g : value.split("\\.")) {
                         groups.add(Integer.parseInt(g));
                     }
                 }
+                case 58 -> lockPlayerX = true;
+                case 59 -> lockPlayerY = true;
                 case 61 -> editorLayer2 = Integer.parseInt(value);
                 case 62 -> spawnTriggered = true;
+                case 64 -> dontFade = true;
+                case 67 -> dontEnter = true;
+                case 71 -> targetPosID = Integer.parseInt(value);
+                case 85 -> easingRate = Float.parseFloat(value);
                 case 96 -> noGlow = true;
+                case 100 -> useTarget = true;
+                case 101 -> xOrY = Integer.parseInt(value);
+                case 103 -> highDetail = true;
+                case 108 -> linkedID = Integer.parseInt(value);
             }
         }
     }
@@ -105,9 +147,19 @@ public class GDObject {
             formatted.append("9," + blue + ",");
         }
         if (moveTime != -1) formatted.append("10," + moveTime + ",");
+        if (id == 901) {  //Move trigger ID
+            formatted.append("28," + moveX + ",");
+            formatted.append("29," + moveY + ",");
+            formatted.append("30," + easingMode + ",");
+        }
         if (touchTriggered) formatted.append("11,1,");
+        if (usePlayerCol1) formatted.append("15,1,");
+        if (usePlayerCol2) formatted.append("16,1,");
+        if (useBlending) formatted.append("17,1,");
         if (editorLayer1 != 0) formatted.append("20," + editorLayer1 + ",");
-        if (colorChannel != -1) formatted.append("21," + colorChannel + ",");
+        if (baseColorChannel != -1) formatted.append("21," + baseColorChannel + ",");
+        if (detailColorChannel != -1) formatted.append("22," + detailColorChannel + ",");
+        if (colorTargetID != -1) formatted.append("23," + colorTargetID + ",");
         if (zLayer != 0) formatted.append("24," + zLayer + ",");
         if (zOrder != null) formatted.append("25," + zOrder + ",");
         if (text != null) formatted.append("31," + Base64Functions.encode(text) + ",");
@@ -118,6 +170,10 @@ public class GDObject {
         if (fadeIn != -1) formatted.append("45," + fadeIn + ",");
         if (fadeHold != -1) formatted.append("46," + fadeHold + ",");
         if (fadeOut != -1) formatted.append("47," + fadeOut + ",");
+        if (copyColorChannel != -1) {
+            formatted.append("50," + copyColorChannel + ",");
+            formatted.append("49," + copyColorHSB + ",");
+        }
         if (targetGroupID != -1) formatted.append("51," + targetGroupID + ",");
 
         if (groups.size() == 1) {
@@ -130,9 +186,21 @@ public class GDObject {
             formatted.append(groups.get(groups.size()-1)).append(",");
         }
 
+        if (lockPlayerX) formatted.append("58,1,");
+        if (lockPlayerY) formatted.append("59,1,");
         if (editorLayer2 != 0) formatted.append("61," + editorLayer2 + ",");
         if (spawnTriggered) formatted.append("62,1,");
+        if (dontFade) formatted.append("64,1,");
+        if (dontEnter) formatted.append("67,1,");
+        if (targetPosID != -1) formatted.append("71," + targetPosID + ",");
+        if (easingRate != -1) formatted.append("85," + easingRate + ",");
         if (noGlow) formatted.append("96,1,");
+        if (useTarget) {
+            formatted.append("100,1,");
+            formatted.append("101," + xOrY + ",");
+        }
+        if (highDetail) formatted.append("103,1,");
+        if (linkedID != -1) formatted.append("108," + linkedID + ",");
 
         if (Manager.debug) System.out.println(formatted.append("end").toString().replace(",end" , ""));
         return formatted.append(";").toString().replace(",;", ";");
@@ -155,11 +223,11 @@ public class GDObject {
         noGlow = true;
     }
 
-    public void setColorChannel(int colorChannel) {
-        this.colorChannel = colorChannel;
+    public void setBaseColorChannel(int baseColorChannel) {
+        this.baseColorChannel = baseColorChannel;
     }
     public void setColorChannel(Color colorChannel) {
-        this.colorChannel = colorChannel.getChannel();
+        this.baseColorChannel = colorChannel.getChannel();
     }
 
     public void setScale(float s) {
